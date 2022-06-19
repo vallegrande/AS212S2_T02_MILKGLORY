8b36dff15fd67eeb850afb1327dfe69d8fde9be8
jefersonmauricio <102843096+jefersonmauricio@users.noreply.github.com>
2022-06-18 21:58:55 -0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function(){
-    let animacion = document.getElementById('animado');
-    let posicionObj1 = animacion.getBoundingClientRect().top;
-    console.log(posicionObj1);
-})
